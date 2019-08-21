7127e0c1ec9a1d7d1631d4b090a37eaf04d402ff
Mitchell O'Hair <mitchellohair@gmail.com>
2019-08-21 00:38:21 -0700

    Cards set up to be 'flipped' later

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 
+$(document).ready(function () {
+  $('.card').on('click', function () {
+    console.log('clicked');
+    var card = $(event.currentTarget);
+    card.toggleClass('flip-card');
+  })
+});
