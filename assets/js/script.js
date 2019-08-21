5a68e4459e827269233e9d77bd59c3ffbe21ebea
Mitchell O'Hair <mitchellohair@gmail.com>
2019-08-21 12:27:08 -0700

    Card shuffle and dynamic card creation is complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,38 @@
-// var cards = ['assets/images/chocolate-scoop.png', 'assets/images/mint-scoop.png', 'assets/images/.png', 'assets/images/.png', 'assets/images/.png', 'assets/images/.png']
+var cards = ['assets/images/chocolate-scoop.png', 'assets/images/mint-scoop.png',
+             'assets/images/pistachio-coil.png', 'assets/images/pistachio-scoop.png',
+             'assets/images/strawberry-coil.png', 'assets/images/vanilla-coil-caramel.png'];
 
-$(document).ready(function(){
-  $('.card').on('click', function(){
+$(document).ready(initializeApp);
+
+function initializeApp () {
+  var deck = cards.concat(cards);
+  shuffle(deck);
+  deck.forEach(constructCards);
+
+
+  // Event handlers
+  $('.card').on('click', function () {
     $(event.currentTarget).addClass('flip-card flipped');
   });
+}
 
-});
+function shuffle(array) {
+  for (var index = array.length - 1; index > 0; index--) {
+    var randomIndex = Math.floor(Math.random() * (index + 1));
+    [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+  }
+}
+
+function constructCards(cardSrc) {
+  var card = $('<div>').addClass('card');
+  var cardBack = $('<div>').addClass('card-back');
+  var backImg = $('<img>').attr('src', 'assets/images/cardback.png');
+  var cardFace = $('<div>').addClass('card-face');
+  var faceImg = $('<img>').attr('src', cardSrc);
+
+  cardBack.append(backImg);
+  cardFace.append(faceImg);
+
+  card.append(cardBack, cardFace);
+  $('.card-container').append(card);
+}
