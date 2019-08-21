8fd6ec070842b39bfd0b2ce09f798bf5b7a0c138
Mitchell O'Hair <mitchellohair@gmail.com>
2019-08-21 15:30:32 -0700

    Correctly flips if match or not

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,50 @@
-var cards = ['assets/images/chocolate-scoop.png', 'assets/images/mint-scoop.png',
-             'assets/images/pistachio-coil.png', 'assets/images/pistachio-scoop.png',
-             'assets/images/strawberry-coil.png', 'assets/images/vanilla-coil-caramel.png'];
+var cardImages = ['assets/images/chocolate-scoop.png', 'assets/images/mint-scoop.png',
+                  'assets/images/pistachio-coil.png', 'assets/images/pistachio-scoop.png',
+                  'assets/images/strawberry-coil.png', 'assets/images/vanilla-coil-caramel.png'];
+var cards = [];
+
 
 $(document).ready(initializeApp);
 
 function initializeApp () {
-  var deck = cards.concat(cards);
+  var deck = cardImages.concat(cardImages);
   shuffle(deck);
   deck.forEach(constructCards);
 
-
   // Event handlers
   $('.card').on('click', function () {
-    $(event.currentTarget).addClass('flip-card flipped');
+    var card = $(event.currentTarget);
+
+    if (card.hasClass('flipped') ||
+        card.hasClass('match')) {
+      return;
+    }
+
+    card.removeClass('card');
+    card.addClass('flip-card flipped');
+
+    cards.push(card);
+
+    if (cards.length === 2) {
+      if (checkMatch(cards[0], cards[1])) {
+        $(cards[0]).removeClass('flipped');
+        $(cards[0]).addClass('match');
+        $(cards[1]).removeClass('flipped');
+        $(cards[1]).addClass('match');
+
+        cards = [];
+      } else {
+        setTimeout(function(){
+          $(cards[0]).removeClass('flip-card flipped');
+          $(cards[0]).addClass('card');
+          $(cards[1]).removeClass('flip-card flipped');
+          $(cards[1]).addClass('card');
+
+          cards = [];
+        }, 1000);
+      }
+
+    }
   });
 }
 
@@ -35,4 +67,13 @@ function constructCards(cardSrc) {
 
   card.append(cardBack, cardFace);
   $('.card-container').append(card);
+}
+
+function checkMatch (card1, card2) {
+  card1 = $(card1)[0].childNodes[1].childNodes[0];
+  card1 = $(card1).attr('src');
+  card2 = $(card2)[0].childNodes[1].childNodes[0];
+  card2 = $(card2).attr('src');
+
+  return card1 === card2;
 }
