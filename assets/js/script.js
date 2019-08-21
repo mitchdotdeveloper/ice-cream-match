9e30620e8e8da2d67e9325e829d060d213d1f7bf
Mitchell O'Hair <mitchellohair@gmail.com>
2019-08-21 12:14:05 -0700

    Layout set

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,8 @@
+// var cards = ['assets/images/chocolate-scoop.png', 'assets/images/mint-scoop.png', 'assets/images/.png', 'assets/images/.png', 'assets/images/.png', 'assets/images/.png']
 
 $(document).ready(function(){
   $('.card').on('click', function(){
-    $(event.currentTarget).toggleClass('flip-card');
-  });
-
-  $('.circle').on('mouseover', function(){
-    $(event.currentTarget).toggleClass('circle-fill');
-    $(event.currentTarget).toggleClass('circle-enlarge');
-  });
-
-  $('.circle').on('mouseout', function () {
-    $(event.currentTarget).toggleClass('circle-fill');
-    $(event.currentTarget).toggleClass('circle-enlarge');
+    $(event.currentTarget).addClass('flip-card flipped');
   });
 
 });
-
-
-// $('.circle').on('click', function () {
-  //   var circle = $(event.currentTarget).toggleClass('circle-enlarge');
-  //   setTimeout(function () {
-  //     $(circle).toggleClass('circle-enlarge');
-  //   }, 1000);
-  // });
