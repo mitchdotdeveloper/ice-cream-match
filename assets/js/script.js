44cbfd69a13f8c8b30d3304a9a5124a74fd6f27e
Mitchell O'Hair <mitchellohair@gmail.com>
2019-08-21 11:59:14 -0700

    Set base layout for states section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,22 @@ $(document).ready(function(){
     $(event.currentTarget).toggleClass('flip-card');
   });
 
+  $('.circle').on('mouseover', function(){
+    $(event.currentTarget).toggleClass('circle-fill');
+    $(event.currentTarget).toggleClass('circle-enlarge');
+  });
+
+  $('.circle').on('mouseout', function () {
+    $(event.currentTarget).toggleClass('circle-fill');
+    $(event.currentTarget).toggleClass('circle-enlarge');
+  });
+
 });
+
+
+// $('.circle').on('click', function () {
+  //   var circle = $(event.currentTarget).toggleClass('circle-enlarge');
+  //   setTimeout(function () {
+  //     $(circle).toggleClass('circle-enlarge');
+  //   }, 1000);
+  // });
